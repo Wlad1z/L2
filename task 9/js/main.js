17af6a3f2e798a65c9df57c4cf28e9a69018a345
Wlad1z <113522178+Wlad1z@users.noreply.github.com>
2023-09-29 09:28:39 +0300

    fix function renderProducts

--- task 9/js/main.js
+++ task 9/js/main.js
@@ -76,28 +76,34 @@ function renderProducts() {
         productList.appendChild(li);
         if (product.getCalories> maxSize){
             maxSize = product.getCalories;
-            const height = 100 / (maxSize / product.getCalories);
-            const div = document.createElement('div');
-        
-            div.style.height = height + "%";
-            div.classList.add('bar');
-            div.innerHTML = product.getCalories + " ккал.";
-            chart.appendChild(div);
-            const hiddenDiv = document.createElement('div');
-            hiddenDiv.innerHTML = product.name + "<br>"+ product.getCalories + " ккал.";
-            if (count > 15){
-                hiddenDiv.style.right = 0;
-            }
-            hiddenDiv.classList.add('hidden', 'placeholder');
-            div.appendChild(hiddenDiv);
-            div.addEventListener('mouseenter', () => {
-                hiddenDiv.classList.remove('hidden');
-            });
-            div.addEventListener('mouseleave', () => {
-                hiddenDiv.classList.add('hidden');
-            });
-            count ++;
         }
+    });
+    products.forEach(product => {
+        const height = 100 / (maxSize / product.getCalories);
+        const div = document.createElement('div');
+    
+        div.style.height = height + "%";
+        div.classList.add('bar');
+        div.innerHTML = product.getCalories + " ккал.";
+        chart.appendChild(div);
+        const hiddenDiv = document.createElement('div');
+        hiddenDiv.innerHTML = product.name + "<br>"+ product.getCalories + " ккал.";
+        if (count > 15){
+            hiddenDiv.style.right = 0;
+        }
+        hiddenDiv.classList.add('hidden', 'placeholder');
+        div.appendChild(hiddenDiv);
+    
+        // Добавить обработчик события для наведения мыши
+        div.addEventListener('mouseenter', () => {
+            hiddenDiv.classList.remove('hidden');
+        });
+    
+        // Добавить обработчик события для убирания мыши
+        div.addEventListener('mouseleave', () => {
+            hiddenDiv.classList.add('hidden');
+        });
+        count ++;
     });
 
     totalCaloriesDiv.textContent = `Суммарно: ${totalCalories} калорий`;
